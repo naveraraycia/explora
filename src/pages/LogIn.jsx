8f5352532645cb2cfd1cbb00bbd31bca4facba4d
aradelraycia <129363766+aradelraycia@users.noreply.github.com>
2023-04-28 19:38:21 +0800

    Fixed the Sign up and Log In pages responsiveness

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -45,56 +45,40 @@ function LogIn() {
 
   return (
     <div className="flex items-center justify-center min-h-screen md:p-10">
-    <div className="relative flex flex-col bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0 md:m-0">
-
-      <div className="p-5 md:p-20">
+     <div className="relative flex flex-col items-center w-full bg-white md:rounded-2xl  lg:shadow-2xl lg:items-stretch lg:flex-row lg:space-y-0 lg:m-0 lg:w-fit">
+      <div className="p-5 w-full max-w-[450px] md:p-10">
         <h1 className="font-sans font-bold text-gray text-4xl mb-5">Log In</h1>
-
         <p className="font-sans font-regular mb-5 text-gray tracking-wider leading-8">Log in to your account to book with Explora!</p>
-
-      <form onSubmit={onSubmit}>
-
-        <div className="max-w-md">
-        <input type="email" className="rounded-lg border border-lightGray mb-5 w-full text-gray p-5 placeholder:font-sans placeholder:font-normal focus:outline-none" placeholder="Email" id='email' value={email} onChange={onChange} />
-
-        <div className="relative flex items-center justify-end">
-          <img src={visibilityIcon} alt='show password' onClick={()=> setShowPassword((prevState) => !prevState)} className="absolute top-5 right-5 text-gray hover:cursor-pointer" />
-
-          <input type={showPassword ? 'text' : 'password'} className="rounded-lg border border-lightGray mb-5 w-full text-gray p-5 placeholder:font-sans placeholder:font-normal focus:outline-none" placeholder="Password" id='password' value={password} onChange={onChange} />
-          
-        </div>
-        </div>
-
-        <div className="flex flex-col items-center justify-between md:flex-row">
-          <Link to='/forgot-password'>
-          <p className="font-sans font-normal my-5 text-blueGreen text-sm tracking-wider hover:text-darkBlueGreen hover:md:scale-110">Forgot password</p>
-          </Link>
-
-          <Button color='blue' type={'submit'}>SIGN IN</Button>
-        </div>
-
-        <p className="font-sans font-normal my-5 text-center text-gray text-sm tracking-wider md:text-left">or log in with</p>
-
-    </form>
-    
+        <form onSubmit={onSubmit}>
+          <div className="max-w-md">
+            <input type="email" className="rounded-lg border border-lightGray mb-5 w-full text-gray p-5 placeholder:font-sans placeholder:font-normal focus:outline-none" placeholder="Email" id='email' value={email} onChange={onChange} />
+            <div className="relative flex items-center justify-end">
+              <img src={visibilityIcon} alt='show password' onClick={()=> setShowPassword((prevState) => !prevState)} className="absolute top-5 right-5 text-gray hover:cursor-pointer" />
+              <input type={showPassword ? 'text' : 'password'} className="rounded-lg border border-lightGray mb-5 w-full text-gray p-5 placeholder:font-sans placeholder:font-normal focus:outline-none" placeholder="Password" id='password' value={password} onChange={onChange} />     
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-between md:flex-row">
+            <Link to='/forgot-password'>
+            <p className="font-sans font-normal my-5 text-blueGreen text-sm tracking-wider hover:text-darkBlueGreen hover:md:scale-110">Forgot password</p>
+            </Link>
+            <Button color='blue' type={'submit'}>SIGN IN</Button>
+          </div>
+          <p className="font-sans font-normal my-5 text-center text-gray text-sm tracking-wider md:text-left">or log in with</p>
+        </form>
         <div className="flex items-center justify-between flex-col md:flex-row">
-
-        <OAuth />
-
-        <Link to='/sign-up'>
-        <p className="font-sans font-normal my-5 text-center text-blueGreen text-sm tracking-wider md:text-left hover:text-darkBlueGreen hover:md:scale-110">Sign Up instead</p>
-        </Link>
+          <OAuth />
+          <Link to='/sign-up'>
+          <p className="font-sans font-normal my-5 text-center text-blueGreen text-sm tracking-wider md:text-left hover:text-darkBlueGreen hover:md:scale-110">Sign Up instead</p>
+          </Link>
         </div>
-
         <div className="my-5"> 
-        <Link to='/'>
-          <p className="font-sans text-gray text-xs hover:cursor-pointer hover:md:underline hover:text-[#373131] inline">Back to home</p>
-        </Link>
+          <Link to='/'>
+            <p className="font-sans text-gray text-xs hover:cursor-pointer hover:md:underline hover:text-[#373131] inline">Back to home</p>
+          </Link>
         </div>
-
       </div>
 
-      <img src={loginPhoto} alt="login" className="w-[430px] hidden md:block md:rounded-r-2xl" />
+      <img src={loginPhoto} alt="login" className="w-[430px] hidden lg:block lg:rounded-r-2xl" />
     </div>
     </div>
   )
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
-import {getAuth, createUserWithEmailAndPassword, updateProfile} from 'firebase/auth'
-import {db} from '../firebase.config'
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import { db } from '../firebase.config'
 import { useNavigate } from 'react-router-dom'
-import {setDoc, doc, serverTimestamp} from 'firebase/firestore'
-import {toast, Flip} from 'react-toastify'
+import { setDoc, doc, serverTimestamp } from 'firebase/firestore'
+import { toast, Flip } from 'react-toastify'
 import OAuth from '../components/OAuth'
-
 import signUpPhoto from '../assets/desktop/signup.jpg'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Button from '../components/shared/Button'
 import visibilityIcon from '../assets/icons/visibilityIcon.svg'
 
@@ -43,7 +42,6 @@ function SignUp() {
         displayName: name
       })
 
-    
       // for saving user to firestore database
       const formDataCopy = {...formData}
 
@@ -62,52 +60,38 @@ function SignUp() {
 
   return (
     <div className="flex items-center justify-center min-h-screen md:p-10">
-    <div className="relative flex flex-col bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0 md:m-0">
-
-      <div className="order-2 p-5 md:p-20">
-        <h1 className="font-sans font-bold text-gray text-4xl mb-5">Sign Up</h1>
-        <p className="font-sans font-regular mb-5 text-gray tracking-wider leading-8">Create an account to start your awesome adventures!</p>
-
-    <form onSubmit={onSubmit}>
-
-        <div className="max-w-md">
-        <input type="text" className="rounded-lg border border-lightGray mb-5 w-full text-gray p-5 placeholder:font-sans placeholder:font-normal focus:outline-none" placeholder="Name" id='name' value={name} onChange={onChange} />
-
-        <input type="email" className="rounded-lg border border-lightGray mb-5 w-full text-gray p-5 placeholder:font-sans placeholder:font-normal focus:outline-none" placeholder="Email" id='email' value={email} onChange={onChange} />
-
-        <div className="relative flex items-center justify-end">
-          <img src={visibilityIcon} alt='show password' onClick={()=> setShowPassword((prevState) => !prevState)} className="absolute top-5 right-5 text-gray hover:cursor-pointer" />
-
-          <input type={showPassword ? 'text' : 'password'} className="rounded-lg border border-lightGray mb-5 w-full text-gray p-5 placeholder:font-sans placeholder:font-normal focus:outline-none" placeholder="Password" id='password' value={password} onChange={onChange} />
-          
-        </div>
-        </div>
-
-        <div className="flex flex-col items-center justify-between md:flex-row">
-          <Link to='/sign-in' className='order-2'>
-          <p className="font-sans font-normal text-blueGreen text-sm tracking-wider hover:text-darkBlueGreen hover:md:scale-110 mt-5 md:mt-0">Log In instead</p>
-          </Link>
-
-          <Button type={'submit'} className="order-1" color='blue'>SIGN UP</Button>
-        </div>
-
-      </form>
+      <div className="relative flex flex-col items-center w-full bg-white md:rounded-2xl  lg:shadow-2xl lg:items-stretch lg:flex-row lg:space-y-0 lg:m-0 lg:w-fit">
+        <div className="order-2 p-5 w-full max-w-[450px] md:p-10">
+          <h1 className="font-sans font-bold text-gray text-4xl mb-5">Sign Up</h1>
+          <p className="font-sans font-regular mb-5 text-gray tracking-wider leading-8">Create an account to start your awesome adventures!</p>
+          <form onSubmit={onSubmit}>
+            <div className="max-w-md">
+              <input type="text" className="rounded-lg border border-lightGray mb-5 w-full text-gray p-5 placeholder:font-sans placeholder:font-normal focus:outline-none" placeholder="Name" id='name' value={name} onChange={onChange} />
+              <input type="email" className="rounded-lg border border-lightGray mb-5 w-full text-gray p-5 placeholder:font-sans placeholder:font-normal focus:outline-none" placeholder="Email" id='email' value={email} onChange={onChange} />
+              <div className="relative flex items-center justify-end">
+                <img src={visibilityIcon} alt='show password' onClick={()=> setShowPassword((prevState) => !prevState)} className="absolute top-5 right-5 text-gray hover:cursor-pointer" />
+                <input type={showPassword ? 'text' : 'password'} className="rounded-lg border border-lightGray mb-5 w-full text-gray p-5 placeholder:font-sans placeholder:font-normal focus:outline-none" placeholder="Password" id='password' value={password} onChange={onChange} />
+              </div>
+            </div>
+            <div className="flex flex-col items-center justify-between md:flex-row">
+              <Link to='/sign-in' className='order-2'>
+              <p className="font-sans font-normal text-blueGreen text-sm tracking-wider hover:text-darkBlueGreen hover:md:scale-110 mt-5 md:mt-0">Log In instead</p>
+              </Link>
+              <Button type={'submit'} className="order-1" color='blue'>SIGN UP</Button>
+            </div>
+          </form>
         <div className="flex mb-5 items-center justify-between flex-col md:flex-row">
-        
-          <p className=" font-sans font-normal my-5 text-center text-gray text-sm tracking-wider md:text-left">or sign up with</p>
-          
+          <p className=" font-sans font-normal my-5 text-center text-gray text-sm tracking-wider md:text-left">or sign up with</p>   
           <OAuth />
         </div>
-
         <div className="my-5"> 
-        <Link to='/'>
-          <p className="font-sans text-gray text-xs hover:cursor-pointer hover:md:underline hover:text-[#373131] inline">Back to home</p>
-        </Link>
+          <Link to='/'>
+            <p className="font-sans text-gray text-xs hover:cursor-pointer hover:md:underline hover:text-[#373131] inline">Back to home</p>
+          </Link>
         </div>
-
       </div>
       
-      <img src={signUpPhoto} alt="login" className="order-1 w-[430px] hidden md:block md:rounded-l-2xl" />
+      <img src={signUpPhoto} alt="login" className="order-1 w-[430px] hidden lg:block lg:rounded-l-2xl" />
     </div>
     </div>
   )
